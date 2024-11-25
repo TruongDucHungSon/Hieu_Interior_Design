8b1f5af96da25b0764e398cd7c0b0bef66da6bd6
DevSon0812 <97348196+DevSon0812@users.noreply.github.com>
2024-11-25 09:40:20 +0700

    add animation

--- src/container/home/index.tsx
+++ src/container/home/index.tsx
@@ -1,19 +1,58 @@
-import Banner from "./_components/SectionBanner";
-import CarouselProduct from "./_components/SectionCarouselProduct";
-import DesignSection from "./_components/SectionDesign";
-import DesignedSection from "./_components/SectionDesigned";
-import ProjectSection from "./_components/SectionProject";
+'use client';
+import { motion } from 'framer-motion';
+import { useEffect, useState } from 'react';
+import { useInView } from 'react-intersection-observer';
+import Banner from './_components/SectionBanner';
+import CarouselProduct from './_components/SectionCarouselProduct';
+import DesignSection from './_components/SectionDesign';
+import DesignedSection from './_components/SectionDesigned';
+import ProjectSection from './_components/SectionProject';
+
+const fadeInVariant = {
+	initial: { opacity: 0, y: 20 },
+	animate: { opacity: 1, y: 0, transition: { duration: 0.6 } },
+};
+
+const SectionWrapper = ({ children }: { children: React.ReactNode }) => {
+	const [ref, inView] = useInView({ triggerOnce: true, threshold: 0.2 });
+	const [hasAnimated, setHasAnimated] = useState(false);
+
+	useEffect(() => {
+		if (inView) setHasAnimated(true);
+	}, [inView]);
+
+	return (
+		<motion.div
+			ref={ref}
+			variants={fadeInVariant}
+			initial="initial"
+			animate={hasAnimated ? 'animate' : 'initial'}
+		>
+			{children}
+		</motion.div>
+	);
+};
 
 const HomePage = () => {
-  return (
-    <main>
-      <Banner />
-      <CarouselProduct />
-      <DesignedSection />
-      <ProjectSection />
-      <DesignSection />
-    </main>
-  );
+	return (
+		<main>
+			<SectionWrapper>
+				<Banner />
+			</SectionWrapper>
+			<SectionWrapper>
+				<CarouselProduct />
+			</SectionWrapper>
+			<SectionWrapper>
+				<DesignedSection />
+			</SectionWrapper>
+			<SectionWrapper>
+				<ProjectSection />
+			</SectionWrapper>
+			<SectionWrapper>
+				<DesignSection />
+			</SectionWrapper>
+		</main>
+	);
 };
 
 export default HomePage;
